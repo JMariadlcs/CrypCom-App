bbf585e82b9d1b2248be44d751b5991a35388213
Paco <franciscoatalayagomez@gmail.com>
2021-03-21 16:58:28 +0100

    Navigation Bar

--- app/src/main/java/com/crypcomapp/FirstFragment.java
+++ app/src/main/java/com/crypcomapp/FirstFragment.java
@@ -23,11 +23,11 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+
+        view.findViewById(R.id.ProfileButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                NavHostFragment.findNavController(FirstFragment.this).navigate(R.id.action_FirstFragment_to_profile);
             }
         });
     }
